5ccb36bf3be6edb2ea8603f27f72eb51d4c53281
tis <tisbootis@gmail.com>
2020-05-15 23:20:46 +0300

    add js for list__more-block

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ const header = document.querySelector('header');
 const width = document.body.clientWidth;
 const height = document.body.clientHeigh;
 const anchors = document.querySelectorAll('a[href*="#"]'); // якоря
+const overlay = document.querySelector('.overlay');
 
 // мобильное меню
 let menuToggle = header.querySelector(".menu-toggle");
@@ -35,4 +36,86 @@ if (width <= 756) {
   //   $('.main-nav__sub-list').not(currentSublist).slideUp();
   //   currentSublist.slideToggle();
   // });
+}
+
+// вкладка "подробнее" у списка
+let moreBtnArr = document.querySelectorAll('.list__more-info');
+for (let moreBtn of moreBtnArr) {
+  let currentMoreBlock = moreBtn.closest('.list__more-wrap').querySelector('.list__more-block');
+  let currentModalClose = currentMoreBlock.querySelector('.list__more-close');
+  let mainMoreBlock = body.querySelector('.list__main-wrap');
+
+  // если элемент в фокусе с клавиатуры - показать вкладку
+  if (width > 756) {
+    moreBtn.onfocus = () => {
+      currentMoreBlock.classList.add('focus');
+      mainMoreBlock.classList.add('tab');
+    };
+
+    moreBtn.onblur = () => {
+      currentMoreBlock.classList.remove('focus');
+      mainMoreBlock.classList.remove('tab');
+    };
+  }
+
+  // отдельное модальное окно
+  if (width <= 756) {
+    moreBtn.addEventListener('click', function(evt) {
+
+      // let currentMoreBlock = moreBtn.nextElementSibling;
+      // let currentMoreBlock = moreBtn.closest('.list__more-wrap').querySelector('.list__more-block');
+      // let currentModalClose = currentMoreBlock.querySelector('.list__more-close');
+
+      openModal(currentMoreBlock);
+
+      currentModalClose.onclick = function() {
+        closeModal(currentMoreBlock);
+      };
+
+      // шаблон
+      // let infoContainer = body.querySelector('.list-more__mobile-container');
+      // let infoTemplate = body.querySelector('#list-more-block').content;
+      // let infoTemplateBlock = infoTemplate.querySelector('.list__more-block');
+
+      // // получаем текст из выбранного элемента
+      // let infoTitleValue = currentMoreBlock.querySelector('.list__more-title').textContent;
+      // let infoTextValue = currentMoreBlock.querySelector('.list__more-text').textContent;
+
+      // // записываем полученные данные в шаблон
+      // let infoTemplateTitle = infoTemplateBlock.querySelector('.list__more-title');
+      // let infoTemplateText = infoTemplateBlock.querySelector('.list__more-text');
+      // infoTemplateTitle.textContent = infoTitleValue;
+      // infoTemplateText.textContent = infoTextValue;
+
+      // // клонируем и добавляем шаблон
+      // let info = infoTemplateBlock.cloneNode(true);
+      // infoContainer.appendChild(info);
+
+      // infoTemplateTitle.textContent = '';
+      // infoTemplateText.textContent = '';
+
+      // // оперделяем ключевые элементы в новом модальном окне
+      // let modalMoreBlock = infoContainer.querySelector('.list__more-block');
+      // let currentModalClose = infoContainer.querySelector('.list__more-close');
+
+      // openModal(modalMoreBlock);
+      // currentModalClose.onclick = function() {
+      //   closeModal(modalMoreBlock);
+      // };
+    })
+  }
+}
+
+function openModal(element) {
+  element.classList.add('active');
+  overlay.classList.add('active');
+  body.classList.add('noscroll');
+}
+
+function closeModal(element) {
+  if (element.classList.contains('active')) {
+    element.classList.remove('active');
+    overlay.classList.remove('active');
+    body.classList.remove('noscroll');
+  }
 }
