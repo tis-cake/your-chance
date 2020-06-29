c99e6b8d46c8a0c1f5fca6aa7e5b43b95c870874
tis <tisbootis@gmail.com>
2020-06-29 16:00:38 +0300

    add js for indefinite quantity swipers on page

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,3 +1,4 @@
+// свайпер фото и врачей
 var centerSwiper = new Swiper('#swiper', {
   slidesPerView: '2',
   spaceBetween: 25,
@@ -17,3 +18,39 @@ var centerSwiper = new Swiper('#swiper', {
     }
   }
 });
+
+function renderSwiper () {
+
+  // параметры свайпера, не включая элементы навигации и скроллбар
+  var swiperParam = {
+    slidesPerView: '2',
+    spaceBetween: 25,
+    touchRatio: 1,
+    navigation: {},
+    scrollbar: {
+      draggable: true,
+    },
+    breakpoints: {
+      756: {
+        slidesPerView: '4',
+        spaceBetween: 30,
+      }
+    }
+  }
+
+  for (var i = 0; i < swiperArr.length; i++) {
+    var swiperCurrent = swiperArr[i];
+    var swiperCurrentID = swiperArr[i].id;
+
+    swiperParam.navigation.nextEl = '#'+swiperCurrentID+'~.swiper__button-next';
+    swiperParam.navigation.prevEl = '#'+swiperCurrentID+'~.swiper__button-prev';
+    swiperParam.scrollbar.el = '#'+swiperCurrentID+' .swiper__scrollbar';
+
+    var newSwiper = new Swiper (swiperCurrent, swiperParam);
+  }
+}
+
+// массив со всеми свайперами на странице
+var swiperArr = document.querySelectorAll('.swiper-container');
+
+renderSwiper();
